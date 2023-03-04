9feadcf27e331ccd5e3daef2652ad4e70c9dc575
LinusePratiwi <pratiwilinus@gmail.com>
2023-03-04 18:00:28 +0700

    deploy

---
+++ precache-manifest.0e3ea9aea96a4e6e988f8fb4d3783d19.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "17eecc60b2ffbe81d447",
+    "url": "/admin/overview/css/app.a5f5a014.css"
+  },
+  {
+    "revision": "17eecc60b2ffbe81d447",
+    "url": "/admin/overview/js/app.9004e429.js"
+  },
+  {
+    "revision": "6f2ef614e03415c09193",
+    "url": "/admin/overview/js/chunk-2d219ff9.1189c811.js"
+  },
+  {
+    "revision": "93e32f5beb2afe07b064",
+    "url": "/admin/overview/css/chunk-vendors.cd2ddb44.css"
+  },
+  {
+    "revision": "93e32f5beb2afe07b064",
+    "url": "/admin/overview/js/chunk-vendors.163cebe9.js"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/admin/overview/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/admin/overview/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/admin/overview/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/admin/overview/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ecc597f01b6b1a179f6e3434532ad174",
+    "url": "/admin/overview/index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/admin/overview/.gitkeep"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/admin/overview/favicon.png"
+  },
+  {
+    "revision": "be74132f42ae1d3502f9a9b9fff68ac1",
+    "url": "/admin/overview/Dashboard.PNG"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/admin/overview/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "99e58416b89637502b40ac8350eed85a",
+    "url": "/admin/overview/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/admin/overview/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/admin/overview/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/admin/overview/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/admin/overview/img/faces/face-6.jpg"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/admin/overview/img/default-avatar.png"
+  },
+  {
+    "revision": "43b98081492ac3bcb4a1fac6cf709403",
+    "url": "/admin/overview/img/faces/tim_vector.jpe"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/admin/overview/img/favicon.png"
+  },
+  {
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/admin/overview/img/faces/face-7.jpg"
+  },
+  {
+    "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
+    "url": "/admin/overview/img/faces/face-4.jpg"
+  },
+  {
+    "revision": "0a337c37f03c0462996b7b03758fa72b",
+    "url": "/admin/overview/img/loading-bubbles.svg"
+  },
+  {
+    "revision": "f575a04ebbb31b5798a4c54783e745a2",
+    "url": "/admin/overview/img/new_logo.png"
+  },
+  {
+    "revision": "d27fbc90c2e644dfdc9765640dc713b9",
+    "url": "/admin/overview/img/mask.png"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/admin/overview/img/sidebar-2.jpg"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/admin/overview/img/sidebar-1.jpg"
+  },
+  {
+    "revision": "cd253e23ed052deeb80b42d2ed772183",
+    "url": "/admin/overview/img/sidebar-3.jpg"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/admin/overview/img/sidebar-4.jpg"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/admin/overview/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "44bf13a71a4db6e15913fe8af9296711",
+    "url": "/admin/overview/img/tim_80x80.png"
+  },
+  {
+    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
+    "url": "/admin/overview/img/vue-logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin/overview/robots.txt"
+  }
+];
